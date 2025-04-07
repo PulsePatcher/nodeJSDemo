5cff3d6adefaae4f8823335f1efb3c93d68e663d
PulsePatcher <jciise@pm.me>
2025-04-07 21:13:47 +0300

    Update prompt text in interactiveCalculator

--- interactiveCalculator.js
+++ interactiveCalculator.js
@@ -3,7 +3,7 @@ const readline = require('readline').createInterface({
     output: process.stdout
   });
   
-  readline.question('Enter the first number: ', (number1) => {
+  readline.question('Pick your first number: ', (number1) => {s
     readline.question('Enter the second number: ', (number2) => {
       const sum = Number(number1) + Number(number2);
       console.log(`The sum of ${number1} and ${number2} is ${sum}`);
