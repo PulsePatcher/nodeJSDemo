678fd7b70534ef0dd1c4899a21c2b3abe9335707
PulsePatcher <jciise@pm.me>
2025-04-07 21:36:44 +0300

    Add subtraction feature to interactive calculator

--- interactiveCalculator.js
+++ interactiveCalculator.js
@@ -10,3 +10,20 @@ const readline = require('readline').createInterface({
       readline.close();
     });
   });
+
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Pick your first number: ', (number1) => {
+    readline.question('Pick your second number: ', (number2) => {
+      const num1 = Number(number1);
+      const num2 = Number(number2);
+      const sum = num1 + num2;
+      const diff = num1 - num2;
+      console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+      console.log(`The difference of ${num1} and ${num2} is ${diff}`);
+      readline.close();
+    });
+  });
